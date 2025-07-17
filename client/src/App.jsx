e4b1792274b9ecc7f09019e997329ec086b4317e
TaeTae-01 <ctj0999@naver.com>
2025-07-17 19:01:26 +0900

    Design: TailwindCSS 적용중

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom'
 import AuthTest from './test/auth/AuthTest'
 import Home from './components/Home';
@@ -7,12 +6,16 @@ function App() {
 
   return (
     <>
-      <nav>
+      <nav className="flex justify-center space-x-4 p-4 bg-gray-100 p-4 dark:bg-gray-900">
         <Link to="/test/auth">
-          <button>AUTH API TEST</button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300 ease-in-out transform hover:scale-105">
+            AUTH API TEST
+          </button>
         </Link>
         <Link to="/home">
-          <button>Go Home</button>
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg transition duration-300 ease-in-out transform hover:scale-105">
+            Go Home
+          </button>
         </Link>
       </nav>
       
